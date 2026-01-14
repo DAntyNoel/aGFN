30c1fd4d2f230ab333df6ca91acea3a5a54c6afe
dantynoel <dantynoel@sjtu.edu.cn>
2026-01-14 20:57:34 +0800

    *third:45

--- 1-set/download_wandb.py
+++ 1-set/download_wandb.py
@@ -30,34 +30,63 @@ def d1():
         
     runs_df.to_csv("rebuttal_set_temp_old.csv")
 
+def d2():
+    # Project is specified by <entity/project-name>
+    runs = api.runs("1969773923-shanghai-jiao-tong-university/Refactored-Alpha-GFN-Set-New-icml")
 
-# Project is specified by <entity/project-name>
-runs = api.runs("1969773923-shanghai-jiao-tong-university/Refactored-Alpha-GFN-Set-New-icml")
+    summary_list, config_list, name_list = [], [], []
+    for run in runs:
+        # .summary contains the output keys/values for metrics like accuracy.
+        #  We call ._json_dict to omit large files
+        summary_list.append(run.summary._json_dict)
 
-summary_list, config_list, name_list = [], [], []
-for run in runs:
-    # .summary contains the output keys/values for metrics like accuracy.
-    #  We call ._json_dict to omit large files
-    summary_list.append(run.summary._json_dict)
+        # .config contains the hyperparameters.
+        #  We remove special values that start with _.
+        config_list.append(
+            {k: v for k,v in run.config.items()
+            if not k.startswith('_')})
 
-    # .config contains the hyperparameters.
-    #  We remove special values that start with _.
-    config_list.append(
-        {k: v for k,v in run.config.items()
-          if not k.startswith('_')})
+        # .name is the human-readable name of the run.
+        name_list.append(run.name)
+        print(run.name)
 
-    # .name is the human-readable name of the run.
-    name_list.append(run.name)
-    print(run.name)
+    runs_df = pd.DataFrame({
+        "summary": summary_list,
+        "config": config_list,
+        "name": name_list
+        })
+        
+    runs_df.to_csv("refactored_alpha_gfn_set_new_icml.csv")
 
-runs_df = pd.DataFrame({
-    "summary": summary_list,
-    "config": config_list,
-    "name": name_list
-    })
-    
-runs_df.to_csv("refactored_alpha_gfn_set_new_icml.csv")
+def d3():
+    # Project is specified by <entity/project-name>
+    runs = api.runs("1969773923-shanghai-jiao-tong-university/Refactored-Alpha-GFN-Set-New-icml-fl0")
+
+    summary_list, config_list, name_list = [], [], []
+    for run in runs:
+        # .summary contains the output keys/values for metrics like accuracy.
+        #  We call ._json_dict to omit large files
+        summary_list.append(run.summary._json_dict)
+
+        # .config contains the hyperparameters.
+        #  We remove special values that start with _.
+        config_list.append(
+            {k: v for k,v in run.config.items()
+            if not k.startswith('_')})
+
+        # .name is the human-readable name of the run.
+        name_list.append(run.name)
+        print(run.name)
+
+    runs_df = pd.DataFrame({
+        "summary": summary_list,
+        "config": config_list,
+        "name": name_list
+        })
+        
+    runs_df.to_csv("refactored_alpha_gfn_set_new_icml_fl0.csv")
 
 if __name__ == '__main__':
     # d1()
-    pass
+    # d2()
+    d3()
--- 1-set/parse.py
+++ 1-set/parse.py
@@ -77,6 +77,44 @@ def d2():
     json.dump(run_dict, open(f"{exp_name}.json", "w"), indent=2)
     print(f"Total {cnt}. Successful parses: {success}, Failed parses: {failed}, length of run_dict: {len(run_dict)}")
 
+def d3():
+    exp_name = 'refactored_alpha_gfn_set_new_icml_fl0'
+
+    runs_pd = pd.read_csv(f"{exp_name}.csv")
+    run_dict = {}
+    failed = 0
+    success = 0
+    cnt = 0
+    for (sum, name) in zip(runs_pd["summary"], runs_pd["name"]):
+        sum_dict = eval(sum)
+        # print(sum_dict.keys())
+        cnt += 1
+        # exit()
+        try:
+            if exp_name == 'refactored_alpha_gfn_set_new_icml_fl0':
+                if sum_dict['step'] != 9999:
+                    continue
+                if sum_dict['training_mode'] != 'online':
+                    continue
+                if sum_dict['use_alpha_scheduler'] != True:
+                    continue
+                if sum_dict['use_grad_clip'] != False:
+                    continue
+                if sum_dict['reward_temp'] != 1:
+                    continue
+                if sum_dict['fl'] != False:
+                    continue
+            run_dict[name] = sum_dict
+            success += 1
+        except KeyError as e:
+            failed += 1
+            print(f"KeyError for run {name}: {e}")
+            print(f"Summary keys: {sum_dict.keys()}")
+
+    json.dump(run_dict, open(f"{exp_name}.json", "w"), indent=2)
+    print(f"Total {cnt}. Successful parses: {success}, Failed parses: {failed}, length of run_dict: {len(run_dict)}")
+
 if __name__ == '__main__':
     # d1()
-    d2()
+    # d2()
+    d3()
