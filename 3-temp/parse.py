d8f221b85f9929c94ec6968b3250bcfe6c6c5c1d
dantynoel <dantynoel@sjtu.edu.cn>
2026-01-15 17:41:53 +0800

    3-temp: 更新数据

--- 3-temp/parse.py
+++ 3-temp/parse.py
@@ -19,14 +19,51 @@ def d1():
             if exp_name == 'rebuttal_set_temp_fixed':
                 if sum_dict['step'] != 9999:
                     continue
-                if sum_dict['training_mode'] != 'online':
-                    continue
-                if sum_dict['use_alpha_scheduler'] != True:
-                    continue
-                if sum_dict['use_grad_clip'] != False:
-                    continue
+                # if sum_dict['training_mode'] != 'online':
+                #     continue
+                # if sum_dict['use_alpha_scheduler'] != True:
+                #     continue
+                # if sum_dict['use_grad_clip'] != False:
+                #     continue
                 # if sum_dict['reward_temp'] != 1:
                 #     continue
+                if sum_dict['fl'] != True:
+                    continue
+            run_dict[name] = sum_dict
+            success += 1
+        except KeyError as e:
+            failed += 1
+            print(f"KeyError for run {name}: {e}")
+            print(f"Summary keys: {sum_dict.keys()}")
+
+    json.dump(run_dict, open(f"{exp_name}.json", "w"), indent=2)
+    print(f"Total {cnt}. Successful parses: {success}, Failed parses: {failed}, length of run_dict: {len(run_dict)}")
+
+def d2():
+    exp_name = 'rebuttal_set_temp_old'
+
+    runs_pd = pd.read_csv(f"{exp_name}.csv")
+    run_dict = {}
+    failed = 0
+    success = 0
+    cnt = 0
+    for (sum, name) in zip(runs_pd["summary"], runs_pd["name"]):
+        sum_dict = eval(sum)
+        # print(sum_dict.keys())
+        cnt += 1
+        # exit()
+        try:
+            if exp_name == 'rebuttal_set_temp_old':
+                if sum_dict['step'] != 9999:
+                    continue
+                # if sum_dict['training_mode'] != 'online':
+                #     continue
+                # if sum_dict['use_alpha_scheduler'] != True:
+                #     continue
+                # if sum_dict['use_grad_clip'] != False:
+                #     continue
+                if sum_dict['reward_temp'] != 1:
+                    continue
                 # if sum_dict['fl'] != True:
                 #     continue
             run_dict[name] = sum_dict
@@ -41,4 +78,5 @@ def d1():
 
 
 if __name__ == '__main__':
-    d1()
+    # d1()
+    d2()
